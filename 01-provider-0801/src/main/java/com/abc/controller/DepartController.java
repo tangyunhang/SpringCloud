94b162940bd8c03c59f3692b49987d39fd127fa7
tangyunhang <18815292164@163.com>
2021-09-07 19:53:59 +0800

    1-introduction

---
+++ 01-provider-0801/src/main/java/com/abc/controller/DepartController.java
@@ -0,0 +1,41 @@
+package com.abc.controller;
+
+import com.abc.bean.Depart;
+import com.abc.service.DepartService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/provider/depart")
+public class DepartController {
+
+    @Autowired
+    private DepartService departService;
+
+    @PostMapping("/save")
+    public boolean saveHandler(@RequestBody Depart depart){
+        return departService.saveDepart(depart);
+    }
+
+    @DeleteMapping("/del/{id}")
+    public boolean delHandler(@PathVariable("id") Integer id){
+        return departService.removeDepartById(id);
+    }
+
+    @PutMapping("update")
+    public boolean updateHandler(@RequestBody Depart depart){
+        return departService.modifyDepart(depart);
+    }
+
+    @GetMapping("/get/{id}")
+    public Depart getHandler(@PathVariable("id") Integer id){
+        return departService.getDepartById(id);
+    }
+
+    @GetMapping("/list")
+    public List<Depart> listHandler(){
+        return departService.listAllDeparts();
+    }
+}
---
+++ 01-provider-0801/src/main/java/com/abc/service/DepartServiceImpl.java
@@ -0,0 +1,53 @@
+package com.abc.service;
+
+import com.abc.bean.Depart;
+import com.abc.repository.DepartRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class DepartServiceImpl implements DepartService{
+    @Autowired
+    DepartRepository departRepository;
+
+    @Override
+    public boolean saveDepart(Depart depart) {
+        //对于save（）参数，根据id的不同，分为三种情况
+        //depart的id为null，save()执行插入操作
+        //depart的id不为null，且DB中存在id，save()执行的是修改操作
+        //depart的id不为null，但DB中不存在id，save()执行插入操作，但其插入后记录的id值并不是这里指定的id，是根据指定id生成策略所生成的id
+        Depart obj = departRepository.save(depart);
+        return obj!=null?true:false;
+    }
+
+    @Override
+    public boolean removeDepartById(Integer id) {
+        if(departRepository.existsById(id)){
+            departRepository.deleteById(id);
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean modifyDepart(Depart depart) {
+        Depart obj = departRepository.save(depart);
+        return obj!=null?true:false;
+    }
+
+    @Override
+    public Depart getDepartById(int id) {
+        if(departRepository.existsById(id)){
+            departRepository.getById(id);
+            return departRepository.getById(id);
+        }
+        return new Depart();
+    }
+
+    @Override
+    public List<Depart> listAllDeparts() {
+        return departRepository.findAll();
+    }
+}
