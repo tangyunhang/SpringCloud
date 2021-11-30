467cc48eb57c3f5194b6614dfeded5bcc92b9a24
tangyunhang <18815292164@163.com>
2021-11-30 14:41:53 +0800

    Sleuth

---
+++ 07-kafka-sleuth-provider-8103/src/main/java/com/abc/AppKafkaProvider8103.java
@@ -0,0 +1,13 @@
+package com.abc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppKafkaProvider8103 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppKafkaProvider8103.class, args);
+    }
+
+}
---
+++ 07-sleuth-provider-8101/src/main/java/com/abc/AppProvider8101.java
@@ -0,0 +1,13 @@
+package com.abc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppProvider8101 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppProvider8101.class, args);
+    }
+
+}
